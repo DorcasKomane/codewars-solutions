41dc32582b6757bdacc4d85f9dd74c24054a4964
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-06-01 14:56:08 +0200

    Added solution to complementary-dna coding challenge 7kyu.

---
+++ complementary-dna.js
@@ -0,0 +1,50 @@
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+
+function DNAStrand(dna){
+
+    dna = dna.toUpperCase();
+
+    if(dna === 'Haskell'){
+      return 'invalid input';
+    }
+    
+    let validStr = dna.split('');
+    
+    let compArr = [];
+
+    validStr.forEach(symbol => {
+        switch(symbol){
+            case 'A':
+                compArr.push('T');
+                break;
+            case 'T':
+                compArr.push('A');
+                break;
+            case 'C':
+                compArr.push('G');
+                break;
+            case 'G':
+                compArr.push('C');
+                break;
+            default:
+                return 'invalid symbol';
+                break;
+        }
+    });
+
+    return compArr.join('');
+}
+
+  console.log(DNAStrand('ATTGC'));
