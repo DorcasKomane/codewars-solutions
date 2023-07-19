2d77709f62bad3838702b9dd32d8d7cf72b6ce19
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-07-19 19:54:29 +0200

    Added solution to sum-of-numbers coding challenge.

--- cw-19-jul-2023/sum-of-numbers.js
+++ cw-19-jul-2023/sum-of-numbers.js
@@ -10,7 +10,7 @@
 // (1, 2) --> 3 (1 + 2 = 3)
 // (0, 1) --> 1 (0 + 1 = 1)
 // (1, 1) --> 1 (1 since both are same)
-// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 0) --> -1 (-1 + 0 = -1) 
 // (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
 // Your function should only return a number, not the explanation about how 
 //you get that number.
@@ -18,12 +18,22 @@
 
 //my solution:
 function getSum(a, b){
-   console.log('Testing...')
 
    let orderedPair = [];
+   let sum = 0;
 
-   
-   
+   orderedPair.push(a, b);
+
+   orderedPair.sort((a,b) => a - b);
+
+   for(let i = orderedPair[0]; i <= orderedPair[1]; i++){
+        if(orderedPair[0] === orderedPair[1]){
+            return orderedPair[0];
+        }else{
+            sum += i;
+        }
+    }
+    return sum;
 }
 
-console.log(getSum(0,-1));
+console.log(getSum( 5, -1));
