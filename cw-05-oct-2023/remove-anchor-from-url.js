79932743521a6da54d70fbcccc4f7e9ab336cf6a
Dorcas Komane <dorcaskomane1@gmail.com>
2023-10-05 17:57:42 +0200

    Added solution to remove-anchor-from-url.js coding challenge.

---
+++ cw-05-oct-2023/remove-anchor-from-url.js
@@ -0,0 +1,13 @@
+// Complete the function/method so that it returns the url with anything after 
+// the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+    const urlIndex = url.indexOf('#');
+    return urlIndex === - 1 ? url : url.slice(0, urlIndex);
+}
+
+console.log(removeUrlAnchor('www.codewars.com/katas/'));
