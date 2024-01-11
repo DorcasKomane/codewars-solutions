8748b5d891fc4451360d58a950ce36e637cb43e5
dorcaskomane <dorcaskomane1@gmail.com>
2024-01-11 20:20:03 +0200

    added partial solution to alphabet-position.js coding challenge.

---
+++ 2024/January/cw-11-jan-2024/alphabet-position.js
@@ -0,0 +1,39 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every 
+// letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't 
+// return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+// STRINGS FUNDAMENTALS
+
+function alphabetPosition(text) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const lowerCaseText = text.toLowerCase();
+    let newStr = ' '; 
+
+    const regex = /[a-z]/g;
+
+    for(let i = 0; i < lowerCaseText.length; i++){
+        let position = alphabet.indexOf(lowerCaseText[i]) + 1;
+        if(lowerCaseText.match(regex)){
+            // console.log(`${position}`);
+            newStr += `${position} `;
+        }
+        else{
+
+        };
+    }
+
+    return newStr;
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."));
