5343efcc73de6e3a3a38a2cf5a55d5c9f861a794
dorcaskomane <dorcaskomane1@gmail.com>
2024-02-14 15:19:57 +0200

    added solution to string-count.js coding challenge.

---
+++ 2024/February/cw-14-feb-2024/string-count.js
@@ -0,0 +1,39 @@
+// Create a function that accepts a string and a single character, 
+// and returns an integer of the count of occurrences the 2nd argument 
+// is found in the first one.
+
+// If no occurrences can be found, a count of 0 should be returned.
+
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+// str_count("Hello", 'o'); // returns 1
+// str_count("Hello", 'l'); // returns 2
+// str_count("", 'z'); // returns 0
+// Notes
+
+// The first argument can be an empty string
+// In languages with no distinct character data type, the second argument 
+// will be a string of length 1.
+
+// FUNDAMENTALS STRINGS
+
+function strCount(str, letter){  
+    const count = {};
+
+    for(let i = 0; i < str.length; i++){
+        let char = str[i];
+
+        if(count[char]){
+            count[char]++;
+        }else {
+            count[char] = 1;
+        }
+    }
+
+    return count[letter] || 0;
+}
+
+console.log(strCount('Hello', 'o'));
+console.log(strCount('Hello', 'l'));
+console.log(strCount('', 'z'));
